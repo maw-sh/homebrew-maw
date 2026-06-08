c229aba025d45ebf00fe5de4802f396f90d8ec49
maw-sh <289041695+maw-sh@users.noreply.github.com>
2026-06-08 13:31:45 +0300

    maw 0.2.9

--- Casks/maw.rb
+++ Casks/maw.rb
@@ -1,7 +1,7 @@
 cask "maw" do
-  version "0.2.8"
-  sha256 "f211a6efb890b1de6004201658e7228b85432fc431ddf033ce461182c6bba4da"
-  url "https://github.com/maw-sh/homebrew-maw/releases/download/v0.2.8/maw-0.2.8.dmg"
+  version "0.2.9"
+  sha256 "f44b8b3dc3c904066dfd761bdbd220e75e5d925dc084f9f41ed7d9c1c845d106"
+  url "https://github.com/maw-sh/homebrew-maw/releases/download/v0.2.9/maw-0.2.9.dmg"
   name "maw.sh"
   desc "A terminal built around the Claude Code agent"
   app "maw.sh.app"
